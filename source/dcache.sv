9bec34836973519cab913c68242d66faf125b0a8
Yutao Hu <hu298@purdue.edu>
2017-10-17 01:25:41 -0400

    dcache

--- source/dcache.sv
+++ source/dcache.sv
@@ -15,5 +15,75 @@ module dcache (
 import cpu_types_pkg::*;
 import diaosi_types_pkg::*;
 
-logic 		hit;
+
+
+Dcache_t l_frame[7:0], r_frame[7:0],l_frame_next,r_frame_next;
+Dstate_t state,next_state;
+logic lru[7:0];
+
+logic w_en_table;
+logic index [2:0];
+
+integer i;
+
+
+always_ff @(posedge CLK, negedge nRST) begin
+	if(!nRST) begin
+		state <= IDLE;
+		for (i = 0; i < 8; i++) begin
+			 l_frame[i].valid <= 0;
+			 l_frame[i].dirty <= 0;
+			 l_frame[i].tag <= 0;
+			 l_frame[i].data1 <= 0;
+			 l_frame[i].data2 <= 0;
+			 r_frame[i].valid <= 0;
+			 r_frame[i].dirty <= 0;
+			 r_frame[i].tag <= 0;
+			 r_frame[i].data1 <= 0;
+			 r_frame[i].data2 <= 0;
+		end
+	end else begin
+		state <= next_state;
+		if (w_en_table) begin
+			if (lru) begin
+				l_frame[index].valid <= l_frame_next.valid;
+				l_frame[index].dirty <= l_frame_next.dirty;
+				l_frame[index].tag <= l_frame_next.tag;
+				l_frame[index].data1 <= l_frame_next.data1;
+				l_frame[index].data2 <= l_frame_next.data2;
+				r_frame[index].valid <= r_frame[index].valid;
+				r_frame[index].dirty <= r_frame[index].dirty;
+				r_frame[index].tag <= r_frame[index].tag;
+				r_frame[index].data1 <= r_frame[index].data1;
+				r_frame[index].data2 <= r_frame[index].data2;
+			end
+			else begin
+				l_frame[index].valid <= l_frame[index].valid;
+				l_frame[index].dirty <= l_frame[index].dirty;
+				l_frame[index].tag <= l_frame[index].tag;
+				l_frame[index].data1 <= l_frame[index].data1;
+				l_frame[index].data2 <= l_frame[index].data2;
+				r_frame[index].valid <= r_frame_next.valid;
+				r_frame[index].dirty <= r_frame_next.dirty;
+				r_frame[index].tag <= r_frame_next.tag;
+				r_frame[index].data1 <= r_frame_next.data1;
+				r_frame[index].data2 <= r_frame_next.data2;
+			end
+		end else begin
+			l_frame[index].valid <= l_frame[index].valid;
+			l_frame[index].dirty <= l_frame[index].dirty;
+			l_frame[index].tag <= l_frame[index].tag;
+			l_frame[index].data1 <= l_frame[index].data1;
+			l_frame[index].data2 <= l_frame[index].data2;
+			r_frame[index].valid <= r_frame[index].valid;
+			r_frame[index].dirty <= r_frame[index].dirty;
+			r_frame[index].tag <= r_frame[index].tag;
+			r_frame[index].data1 <= r_frame[index].data1;
+			r_frame[index].data2 <= r_frame[index].data2;
+		end		
+	end
+end
+
+
+
 
--- source/icache.sv
+++ source/icache.sv
@@ -16,10 +16,8 @@ module icache (
 import cpu_types_pkg::*;
 import diaosi_types_pkg::*;
 
-logic 		valid,hit;
+logic 		hit;
 integer     i;
-logic  [25:0] tag;
-logic  [31:0] data;
 
 
 
@@ -27,39 +25,40 @@ icachef_t iaddr;
 assign iaddr.tag = icf.iload[31:6];
 assign iaddr.idx = icf.iload[5:2];
 assign iaddr.bytoff = icf.iload[1:0];
-assign dcif.imemload = instr[iaddr.idx].data;
+assign dcif.imemload = frame[iaddr.idx].data;
 assign icf.iaddr =  dcif.imemaddr;
 assign dcif.ihit = hit;
 
-Icache_t instr [15:0];
+Icache_t frame [15:0];
+Icache_t next_frame;
 Istate_t state, next_state;
 
 always_ff @(posedge CLK, negedge nRST) begin
 	if(!nRST) begin
 		state <= IDLE_I;
 		for (i = 0; i < 16; i++) begin
-			 instr[i].tag <= 0;
-			 instr[i].data <= 0;
-			 instr[i].valid <= 0;
+			 frame[i].tag <= 0;
+			 frame[i].data <= 0;
+			 frame[i].valid <= 0;
 		end
 	end else begin
 		state <= next_state;
 		if (hit) begin
-			instr[iaddr.idx].tag 	<= instr[iaddr.idx].tag;
-			instr[iaddr.idx].data   <= instr[iaddr.idx].data;
-			instr[iaddr.idx].valid  <= instr[iaddr.idx].valid;
+			frame[iaddr.idx].tag 	<= frame[iaddr.idx].tag;
+			frame[iaddr.idx].data   <= frame[iaddr.idx].data;
+			frame[iaddr.idx].valid  <= frame[iaddr.idx].valid;
 		end else begin
-			instr[iaddr.idx].tag 	<= tag;
-			instr[iaddr.idx].data   <= data;
-			instr[iaddr.idx].valid  <= valid;
+			frame[iaddr.idx].tag 	<= next_frame.tag;
+			frame[iaddr.idx].data   <= next_frame.data;
+			frame[iaddr.idx].valid  <= next_frame.valid;
 		end		
 	end
 end
 
 always_comb begin
 	hit = 0;
-	if (instr[iaddr.idx].tag == iaddr.tag) begin
-		if (instr[iaddr.idx].valid) begin
+	if (frame[iaddr.idx].tag == iaddr.tag) begin
+		if (frame[iaddr.idx].valid) begin
 			hit = 1;
 		end 
 	end
@@ -68,25 +67,25 @@ end // always_comb
 always_comb begin
 	icf.iREN = 0;
 	if (dcif.imemREN == 0) begin
-		tag = instr[iaddr.idx].tag;
-		data = instr[iaddr.idx].data;
-		valid = instr[iaddr.idx].valid;
+		next_frame.tag = frame[iaddr.idx].tag;
+		next_frame.data = frame[iaddr.idx].data;
+		next_frame.valid = frame[iaddr.idx].valid;
 	end else begin
 		casez (state) 
 		IDLE_I:
 		begin
-		tag = iaddr.tag;
-		data = icf.iload;
-		valid = instr[iaddr.idx].valid;	
+		next_frame.tag = iaddr.tag;
+		next_frame.data = icf.iload;
+		next_frame.valid = frame[iaddr.idx].valid;	
 		if (!hit)
 			next_state = LD;
 		end
 		LD:
 		begin
 			icf.iREN = 1;
-			tag = instr[iaddr.idx].tag;
-			data = instr[iaddr.idx].data;
-			valid = 1;	
+			next_frame.tag = frame[iaddr.idx].tag;
+			next_frame.data = frame[iaddr.idx].data;
+			next_frame.valid = 1;	
 			if (!icf.iwait)
 				next_state = IDLE_I;
 		end
